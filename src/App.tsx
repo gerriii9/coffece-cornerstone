7643217150ab00b0e3e73c24d804a0f13d1ac01f
gerriii9 <140399995+gerriii9@users.noreply.github.com>
2026-03-24 11:02:28 +0100

    Fix import statements for page components

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import index from "./pages/Index";
-import zlatystandard from "./pages/ZlatyStandard";
-import original from "./pages/Original";
-import notfound from "./pages/NotFound";
+import Index from "./pages/index";
+import ZlatyStandard from "./pages/zlatystandard";
+import Original from "./pages/original";
+import NotFound from "./pages/notfound";
 
 const queryClient = new QueryClient();
 
@@ -20,7 +20,6 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/zlaty-standard" element={<ZlatyStandard />} />
           <Route path="/original" element={<Original />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
